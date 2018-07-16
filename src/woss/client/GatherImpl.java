1fbac7d9f83e1f91662752af1e64ac4f7df25db5
phoenix1911 <thisistjl@outlook.com>
2018-07-16 16:15:47 +0800

    gatherimpl

--- src/woss/client/GatherImpl.java
+++ src/woss/client/GatherImpl.java
@@ -14,8 +14,23 @@ public class GatherImpl implements Gather {
 
 	/*
 	 * 采集方法
-	 * 功能:
-	 */
+	 * 功能：从数据文件中获取到一共数据清单
+	 * 实现思路：
+	 * 1.准备一共List集合，用来存放BIDR对象
+	 * 2.准备两个Map集合
+	 * 		1)第一个 标志第一个对象
+	 * 		7	IP,1	第一个用户
+	 * 		7	IP,2
+	 * 		7	IP,3
+	 * 		8	IP,3
+	 * 		8	IP,2
+	 * 		8	IP,1	最后一个用户
+	 * 
+	 * 		2)存放不完整的BIDR对象
+	 * 			上线的信息
+	 * 			下线的信息
+	 * 
+	 * */
 	@Override
 	public Collection<BIDR> gather() throws Exception {
 
