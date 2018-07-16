f27f71f3590c8ca662617fb7743280a1bc2bf9fb
phoenix1911 <thisistjl@outlook.com>
2018-07-16 16:06:18 +0800

    test

---
+++ src/woss/client/GatherImpl.java
@@ -0,0 +1,25 @@
+package woss.client;
+
+import java.util.Collection;
+import java.util.Properties;
+
+import com.briup.util.BIDR;
+import com.briup.woss.client.Gather;
+
+public class GatherImpl implements Gather {
+
+	@Override
+	public void init(Properties arg0) {
+	}
+
+	/*
+	 * 采集方法
+	 * 功能:
+	 */
+	@Override
+	public Collection<BIDR> gather() throws Exception {
+
+		return null;
+	}
+
+}
