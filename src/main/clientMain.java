3f5986813f9621053d651c24ee5756f51237fcaf
phoenix1911 <thisistjl@outlook.com>
2018-07-20 11:05:49 +0800

    配置模块

--- src/main/clientMain.java
+++ src/main/clientMain.java
@@ -8,10 +8,10 @@ public class clientMain {
 	public static void main(String[] args) throws Exception {
 		// 客户端对象 -->调用send方法传入集合.
 		// 采集模块对象-->获取集合
-		
+
 //		需要先创建一个配置文件对象
 		ConfigurationImpl conf = new ConfigurationImpl();
-		
+
 		GatherImpl2 gatherImpl2 = (GatherImpl2) conf.getGather();
 		ClientImpl clientImpl = (ClientImpl) conf.getClient();
 
